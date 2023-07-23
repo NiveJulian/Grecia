87c363966494ac8b591c7cb53823f39e0ef0d56c
niveyrojulian5@gmail.com <niveyrojulian5@gmail.com>
2023-07-23 11:26:12 -0300

    combios necesarios

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('HOLA MUNDO!')
 })
 
 app.listen(port, () => {
-  console.log(`Aplicacion esta en puerto: ${port}`)
+  console.log(`Aplicacion esta escuchando en puerto: ${port}`)
 })
